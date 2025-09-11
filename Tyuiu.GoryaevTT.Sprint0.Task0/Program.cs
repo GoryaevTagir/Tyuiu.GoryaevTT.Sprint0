320f907df9b18e07d09c95d3d42c9e8821799eab
lASTRAYl <Lobstera134@gmail.com>
2025-09-11 21:36:30 +0500

    Add project files.

---
+++ Tyuiu.GoryaevTT.Sprint0.Task0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.GoryaevTT.Sprint0.Task0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello, world!");
+            string name = "Тагир";
+            int age = 17;
+            Console.WriteLine("Здравствуйте, мое имя: " + name + " мне " + age + " лет.");
+            Console.ReadKey();
+        }
+    }
+}
